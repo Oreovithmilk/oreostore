11c714def8f280595ee25e050ddadbe17555e39c
Oreovithmilk <120811481+Oreovithmilk@users.noreply.github.com>
2022-12-17 22:49:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mobile Legends | Recharge Diamonds</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700&family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <nav>
+        <div class="container">
+            <div class="left-section">           
+                 <a href="#"><img class="store-name-nav" src="images/store-name.png" ></a>
+            </div>
+            <div class="right-section">
+                <ul id="nav-links" class="main-menu">
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="#">GAME</a></li>
+                    <li><a href="#">ABOUT US</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+                <div class="search">
+                    <input class="main-search-bar" type="text" placeholder="Search...">
+                    <a href="#">
+                        <i class="search-icon fa-solid fa-magnifying-glass"></i>
+                    </a>
+                </div>
+                <a href="#">
+                    <i class="search-icon fa-solid fa-magnifying-glass search-icon-hide"></i>
+                </a>
+                <a href="#">
+                    <i id="hamburger-menu" class="fa-solid fa-bars"></i>
+                </a> 
+
+                    <!-- <a href="checkout.html">
+                        <img src="images/shopping-bag (1).png">
+                    </a> -->
+            </div>
+        </div>
+    </nav>
+
+    <!--Main---------------------------------------------->
+
+    <main class="mlbb-main">
+        <div class="mlbb-banner-text">
+            <img src="images/floryn-banner.jpg">
+            <div class="banner-text">
+                <h2>MOBILE LEGENDS TOP UP</h2>
+                <div>
+                    <i class="fa-light fa-circle-bolt" style="color:white;"></i>
+                    <p>Instant Delivery</p>
+                </div>
+            </div>
+        </div>
+        <div class="container mlbb-container">
+            <div class="left-section">
+            <div class="diamonds-grid radio-toolbar">
+                <input type="radio" id="dias-14" name="radioDias" value="14 24">
+                <label  class="diamond-card" for="dias-14">
+                        <div class="price-header">
+                            <h4 class="price">₹ 24</h4>
+                            <img src="images/dias.png">
+                        </div>
+                        <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <input type="radio" id="2" name="radioDias" value="" >
+                <label class="diamond-card" for="2">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> <span class="diamonds"> Diamonds</span></p>
+                </label>
+                <input type="radio" id="3" name="radioDias" value="" >
+                <label class="diamond-card" for="3">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <input type="radio" id="4" name="radioDias" value="" >
+                <label class="diamond-card" for="4">
+                        <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label><label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+                <label class="diamond-card" for="dias-14">
+                    <div class="price-header">
+                        <h4 class="price">₹ 24</h4>
+                        <img src="images/dias.png">
+                    </div>
+                    <p class="actual">13 <span>+ 1</span> Diamonds</p>
+                </label>
+        </div>
+            </div>
+            <div id="to-pay" class="right-section">
+                <form id="mlbb-form" action="#">
+                    <div class="order-info">
+                        <h5>User Information<span style="color: red;">*</span></h5>
+                        <input  id="user-id" type="text" placeholder="Enter your user ID" required>
+                        <input id="zone-id" type="text" placeholder="Enter your Zone ID" required>
+                    </div>
+                    <div class="buy-container">
+                        <div class="total">
+                            <h3 >Total</h3>
+                            <h3 id="total" class="price">-</h3>
+                        </div>
+                        <label for="mobile" style="font-size: 12px;">Enter Mobile no.<span style="color: red;">*</span></label>
+                        <div class="mobile">
+                            <input id="mobile" type="text" required>
+                            <p>+91</p>
+                        </div>
+                        <input class="btn buy-now" type="submit" value="BUY NOW">
+                    </div>
+                </form> 
+            </div>
+        </div>
+    </main>
+    <!------------Modal box------------------------------------>
+    <div id="modal-overlay">
+        <div id="modal-box">
+            <div class="modal-header">
+                    <i class="fa-regular fa-circle-check"></i>
+                    <h2>Your order has been successfuly placed!</h2>
+                    <a href="index.html"><i class="fa-solid fa-circle-xmark exit"></i> </a>           
+            </div>
+            <div class="modal-details">
+                <h3>Mobile Legends Diamond</h3>
+                <p>Diamond : <span id="modal-diamond"></span></p>
+                <p>User Id : <span id="modal-user-id"></span></p>
+                <p>Server Id : <span id="modal-zone-id"></span></p>
+            </div>
+           
+            <p>( If you haven't completed the payment yet, <a style="color: red ;" href="#">Click here</a> )</p>
+        </div>
+    </div>
+    <!--Footer------------->
+    <footer>
+        <div class="container">
+                <div class="card payment-head">
+                <h4>Payment Methods </h4>
+                <ul class="payment">
+                    <li><a href="#"><img src="images/Google.svg" alt=""></a></li>
+                    <li><a href="#"><img src="images/Paytm.svg" alt=""></a></li>
+                    <li><a href="#"><img src="images/PhonePe.svg" alt=""></a></li>
+                    <li><a href="#"><img src="images/upi.svg" alt=""></a></li>
+                </ul>
+                </div>
+                <div class="card">
+                    <h4>Our Store</h4>
+                    <ul>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms & Conditons</a></li>
+                        <li><a href="#">Refund Policy</a></li>
+                    </ul>
+                 </div>
+                 <div class="card">
+                    <h4>Support</h4>
+                    <ul>
+                        <li><a href="#">E-mail</a></li>
+                        <li><a href="#">WhatsApp</a></li>
+                    </ul>
+                 </div>
+                 <img class="store-logo" src="images/store-name.png" >
+
+        </div>
+    </footer>
+
+    <script src="https://kit.fontawesome.com/7de94b01f8.js" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+</html>
